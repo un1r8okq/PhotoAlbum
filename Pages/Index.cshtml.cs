2fcbdac92ae7affea69302b4c340c19ee2cf3d8e
Will Verland <will@ver.land>
2022-02-10 08:09:38 +1300

    Add ASP.NET Razor app that shows images from a dir

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoAlbum.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<string> ImgUrls;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            ImgUrls = new List<string>();
+        }
+
+        public void OnGet()
+        {
+            var imgPaths = Directory.GetFiles(Path.Combine("wwwroot", "img"));
+            var imgNames = imgPaths.Select(path => Path.GetFileName(path));
+            ImgUrls = imgNames.Select(imgName => "/img/" + imgName);
+        }
+    }
+}
